74abf65d928e8195ffde9a58e2eb56e0d92e2559
ning1635 <609329119@qq.com>
2016-10-08 19:38:34 +0800

    Create MyValue.java

---
+++ mavenProject/winTest/src/main/java/org/cn/test/mavenPreject/MyValue.java
@@ -0,0 +1,10 @@
+package org.cn.test.mavenProject;
+public class MyValue {
+	public String name;
+	public String age;
+	
+	public String toString(){
+		return "name:" + this.name + ",age:" + this.age + ".";
+	}
+
+}
