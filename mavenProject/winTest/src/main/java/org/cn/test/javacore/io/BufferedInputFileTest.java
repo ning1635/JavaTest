41e02d1c0ca7fde9f89c02d40b390992c2cb9d6c
ning1635 <609329119@qq.com>
2016-11-09 19:14:14 +0800

    Create BufferedInputFileTest.java

---
+++ mavenProject/winTest/src/main/java/org/cn/test/javacore/io/BufferedInputFileTest.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * @author 
+ * @since
+ * read file of line
+ */
+public class BufferedInputFileTest{
+
+	public static String read(String filename) throws IOException{
+		//read file
+		BufferedReader in = new BufferedReader(new FileReader(filename));
+		String s;
+		StringBuilder sb = new StringBuilder();
+		while((s = in.readLine()) != null)
+			sb.append(s + "\n");
+		in.close();
+		return sb.toString();
+	}
+
+	public static void main(String []args) throws IOException{
+		System.out.println(read("1.txt"));
+	}
+}
